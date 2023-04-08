5b6b0802aa57c1fed815e84d6b1086d3310ecdd5
Viraj76 <virajg.7680@gmail.com>
2023-04-09 02:03:24 +0530

    Showing the employees to the boss

--- app/src/main/java/com/example/assigntodo/BossMainActivity.kt
+++ app/src/main/java/com/example/assigntodo/BossMainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.assigntodo
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.BoringLayout.make
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.assigntodo.adapter.BossMainActivityAdapter
+import com.example.assigntodo.databinding.ActivityBossMainBinding
+import com.example.assigntodo.models.Boss
+import com.example.assigntodo.models.Employees
+import com.example.assigntodo.utils.Config
+import com.google.firebase.database.*
 
 class BossMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBossMainBinding
+    private lateinit var bossMainActivityAdapter: BossMainActivityAdapter
+    private lateinit var empDb : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_boss_main2)
+        binding = ActivityBossMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Config.showDialog(this)
+        preparerRvForBossMainActivity()
+        showingAllEmployees()
+    }
+
+    private fun showingAllEmployees() {
+        empDb = FirebaseDatabase.getInstance().getReference("Employees")
+        empDb .addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val empList = arrayListOf<Employees>()
+                for(employees in snapshot.children){
+                    val empData = employees.getValue(Employees::class.java)
+                    empList.add(empData!!)
+                }
+                Config.hideDialog()
+                bossMainActivityAdapter.setEmpList(empList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
+    }
+
+    private fun preparerRvForBossMainActivity() {
+        bossMainActivityAdapter = BossMainActivityAdapter(this)
+        binding.empRv.apply {
+            layoutManager  = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
+            adapter = bossMainActivityAdapter
+        }
     }
 }
--- app/src/main/java/com/example/assigntodo/WorkActivity.kt
+++ app/src/main/java/com/example/assigntodo/WorkActivity.kt
@@ -1,11 +1,32 @@
 package com.example.assigntodo
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.example.assigntodo.databinding.ActivityWorkBinding
+import com.google.firebase.database.R
 
 class WorkActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWorkBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_work2)
+        binding = ActivityWorkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvBlank.text = "No any work has been assigned to ${intent.getStringExtra("EmpName")}"
+        binding.workTb.apply {
+            title = intent.getStringExtra("EmpName")
+            setSupportActionBar(this)
+            setNavigationOnClickListener {
+                startActivity(Intent(context,BossMainActivity::class.java))
+            }
+        }
+        binding.fabAssignWork.setOnClickListener {
+            binding.tvBlank.visibility = View.GONE;
+        }
     }
+
 }
